81ceaadee5a8913d6d16f8c627f270fccf9ce275
Peter Hunt <pehunt@redhat.com>
2020-12-03 16:23:38 -0500

    container_reopen_log: use IsAlive() instead of State()
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/container_reopen_log.go
+++ server/container_reopen_log.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"fmt"
-
-	"github.com/cri-o/cri-o/internal/oci"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
@@ -16,13 +13,8 @@ func (s *Server) ReopenContainerLog(ctx context.Context, req *pb.ReopenContainer
 		return nil, errors.Wrapf(err, "could not find container %s", req.ContainerId)
 	}
 
-	if err := s.ContainerServer.Runtime().UpdateContainerStatus(c); err != nil {
-		return nil, err
-	}
-
-	cState := c.State()
-	if !(cState.Status == oci.ContainerStateRunning || cState.Status == oci.ContainerStateCreated) {
-		return nil, fmt.Errorf("container is not created or running")
+	if err := c.IsAlive(); err != nil {
+		return nil, errors.Errorf("container is not created or running: %v", err)
 	}
 
 	if err := s.ContainerServer.Runtime().ReopenContainerLog(c); err != nil {
