6d1c38bdd7237d00020b15f76deac2beea5f2b00
Peter Hunt <pehunt@redhat.com>
2021-12-16 17:00:59 -0500

    cmdrunner: create singleton
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- utils/cmdrunner/cmdrunner.go
+++ utils/cmdrunner/cmdrunner.go
@@ -4,12 +4,53 @@ import (
 	"os/exec"
 )
 
+// Use a singleton instance because there are many modules that may want access
+// and having it all go through the config is cumbersome.
+var commandRunner CommandRunner
+
 type CommandRunner interface {
-	CombinedOutput(string, ...string) ([]byte, error)
+	command(string, ...string) *exec.Cmd
+	combinedOutput(string, ...string) ([]byte, error)
 }
 
-type RealCommandRunner struct{}
+type RealCommandRunner struct {
+	prependCmd  string
+	prependArgs []string
+}
 
-func (c *RealCommandRunner) CombinedOutput(command string, args ...string) ([]byte, error) {
-	return exec.Command(command, args...).CombinedOutput()
+// Set updates the singleton object with the configured prepended cmd and args
+func Set(prependCmd string, prependArgs ...string) {
+	commandRunner = &RealCommandRunner{
+		prependCmd:  prependCmd,
+		prependArgs: prependArgs,
+	}
+}
+
+func CombinedOutput(command string, args ...string) ([]byte, error) {
+	if commandRunner == nil {
+		return exec.Command(command, args...).CombinedOutput()
+	}
+	return commandRunner.combinedOutput(command, args...)
+}
+
+func (c *RealCommandRunner) combinedOutput(command string, args ...string) ([]byte, error) {
+	return c.command(command, args...).CombinedOutput()
+}
+
+func Command(cmd string, args ...string) *exec.Cmd {
+	if commandRunner == nil {
+		return exec.Command(cmd, args...)
+	}
+	return commandRunner.command(cmd, args...)
+}
+
+func (c *RealCommandRunner) command(cmd string, args ...string) *exec.Cmd {
+	realCmd := cmd
+	realArgs := args
+	if c.prependCmd != "" {
+		realCmd = c.prependCmd
+		realArgs = append(c.prependArgs, cmd)
+		realArgs = append(realArgs, args...)
+	}
+	return exec.Command(realCmd, realArgs...)
 }
