7be46547aa54975d932c102fc8df264abaf08bc9
Peter Hunt <pehunt@redhat.com>
2022-07-21 14:57:52 -0400

    add checkpoint
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/cri/v1/rpc_checkpoint_container.go
+++ server/cri/v1/rpc_checkpoint_container.go
@@ -6,18 +6,8 @@ import (
 	pb "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
 
-func (s *service) MetricsEndpoints(
-	ctx context.Context, req *pb.MetricsEndpointsRequest,
-) (*pb.MetricsEndpointsResponse, error) {
-	return &pb.MetricsEndpointsResponse{
-		Endpoints: []*pb.MetricsEndpoint{
-			{
-				Host: "localhost:" + string(s.server.Config().MetricsConfig.MetricsPort),
-				// Path the Kubelet will register to pull from the source.
-				ForwardPath: "/metrics/cadvisor",
-				// Path the runtime will register as the source.
-				OriginPath: "/metrics",
-			},
-		},
-	}, nil
+func (s *service) CheckpointContainer(
+	ctx context.Context, req *pb.CheckpointContainerRequest,
+) (*pb.CheckpointContainerResponse, error) {
+	return nil, nil
 }
